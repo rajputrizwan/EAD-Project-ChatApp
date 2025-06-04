cd9ed27c1b4d7ff9e76941b4a11d245d6eff1f7d
Muhammad Rizwan <mrizwan2702@gmail.com>
2025-06-04 10:11:38 +0500

    RoutesSetup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+  res.send("Sign-Up");
+};
+
+export const login = (req, res) => {
+  res.send("Log-In");
+};
+
+export const logout = (req, res) => {
+  res.send("Log-Out");
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,13 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.use("/api/auth", authRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on Port: ${process.env.PORT}`);
 });
