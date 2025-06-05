64685d746d59ea13c1c9fe067e47b1a34fac5782
Muhammad Rizwan <mrizwan2702@gmail.com>
2025-06-05 10:15:10 +0500

    profilePicBackendSetup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,20 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(process.env.PORT, () => {
+  connectDB();
   console.log(`Server is running on Port: ${process.env.PORT}`);
 });
