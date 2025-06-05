47f93d3a1ebf3f923caaf5eae1df7fc2e18201b6
Muhammad Rizwan <mrizwan2702@gmail.com>
2025-06-05 19:20:45 +0500

    backendIsAlmostComplete

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 
@@ -13,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(process.env.PORT, () => {
   connectDB();
