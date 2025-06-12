af8ef92dfa4c0a59d345235ddf47e53154678e14
Muhammad Rizwan <mrizwan2702@gmail.com>
2025-06-12 14:32:11 +0500

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,11 @@ import cors from "cors";
 import { app, server } from "./lib/socket.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path from "path";
 
 dotenv.config();
 
+const __dirname = path.resolve();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
@@ -22,6 +24,14 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static([path.join(__dirname, "../fronted/dist")]));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(process.env.PORT, () => {
   connectDB();
